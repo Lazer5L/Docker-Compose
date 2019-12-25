cc0952ff7f8ea70db3076af5c174fe5616e8494c
Brent Erickson <brenteri@gmail.com>
2019-12-25 15:41:06 -0600

    Added stop, copy and up -d

--- BackUpDate.sh
+++ BackUpDate.sh
@@ -1,7 +1,28 @@
 #!/bin/sh
+
+#Pull latest versions of containers
 docker-compose -f /Docker-Compose/guacozy/docker-compose.yml pull
 docker-compose -f /Docker-Compose/deluge/docker-compose.yml pull
 docker-compose -f /Docker-Compose/nzbget/docker-compose.yml pull
 docker-compose -f /Docker-Compose/ombi/docker-compose.yml pull
 docker-compose -f /Docker-Compose/sonarr/docker-compose.yml pull
 docker-compose -f /Docker-Compose/unifi/docker-compose.yml pull
+
+#Stop running containers
+docker-compose -f /Docker-Compose/guacozy/docker-compose.yml stop
+docker-compose -f /Docker-Compose/deluge/docker-compose.yml stop
+docker-compose -f /Docker-Compose/nzbget/docker-compose.yml stop
+docker-compose -f /Docker-Compose/ombi/docker-compose.yml stop
+docker-compose -f /Docker-Compose/sonarr/docker-compose.yml stop
+docker-compose -f /Docker-Compose/unifi/docker-compose.yml stop
+
+#Backup all config directories and files
+cp -R /docker/ /mnt/nas/bk/docker
+
+#Start all containers
+docker-compose -f /Docker-Compose/guacozy/docker-compose.yml up -d
+#docker-compose -f /Docker-Compose/deluge/docker-compose.yml up -d
+#docker-compose -f /Docker-Compose/nzbget/docker-compose.yml up -d
+docker-compose -f /Docker-Compose/ombi/docker-compose.yml up -d
+#docker-compose -f /Docker-Compose/sonarr/docker-compose.yml up -d
+docker-compose -f /Docker-Compose/unifi/docker-compose.yml up -d
