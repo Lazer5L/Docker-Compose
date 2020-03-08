c016e5826ad432a4910414a935fb6900fa2d4311
Brent Erickson <brenteri@gmail.com>
2020-03-08 15:33:53 -0500

    Merge

--- BackUpDate.sh
+++ BackUpDate.sh
@@ -28,17 +28,17 @@ cp /docker/ssh/acme/LetsEncryptTOB.crt /docker/ssh/acme/cert.crt
 cp /docker/ssh/acme/LetsEncryptTOB.key /docker/ssh/acme/cert.key
 
 #Backup all config directories and files
-rsync --archive --verbose --recursive --dry-run --checksum /docker/ /mnt/nas/bk
+rsync --archive --verbose --recursive --checksum /docker/ /mnt/nas/bk
 
 #Start all containers
 docker-compose -f /Docker-Compose/guacozy/docker-compose.yml up -d
-docker-compose -f /Docker-Compose/deluge/docker-compose.yml up -d
-docker-compose -f /Docker-Compose/nzbget/docker-compose.yml up -d
+#docker-compose -f /Docker-Compose/deluge/docker-compose.yml up -d
+#docker-compose -f /Docker-Compose/nzbget/docker-compose.yml up -d
 docker-compose -f /Docker-Compose/ombi/docker-compose.yml up -d
-docker-compose -f /Docker-Compose/sonarr/docker-compose.yml up -d
+#docker-compose -f /Docker-Compose/sonarr/docker-compose.yml up -d
 docker-compose -f /Docker-Compose/unifi/docker-compose.yml up -d
-docker-compose -f /Docker-Compose/plex/docker-compose.yml up -d
-docker-compose -f /Docker-Compose/sftp/docker-compose.yml up -d
+#docker-compose -f /Docker-Compose/plex/docker-compose.yml up -d
+#docker-compose -f /Docker-Compose/sftp/docker-compose.yml up -d
 
 #Prune old images
 docker image prune -f
---
+++ sftp/scripts/bindmount.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+# File mounted as: /etc/sftp.d/bindmount.sh
+# Just an example (make your own)
+
+function bindmount() {
+    if [ -d "$1" ]; then
+        mkdir -p "$2"
+    fi
+    mount --bind $3 "$1" "$2"
+}
+
+# Remember permissions, you may have to fix them:
+# chown -R :users /data/common
+
+bindmount /data/admin-tools /home/admin/tools
+bindmount /data/common /home/dave/common
+bindmount /data/common /home/peter/common
+bindmount /data/docs /home/peter/docs --read-only
