00dd7345a2251e44b5ce353bdd1fcc7b3c05701b
Brent Erickson <brenteri@gmail.com>
2020-04-25 18:04:15 -0500

    Testing foreach loop

--- BackUpDate.sh
+++ BackUpDate.sh
@@ -3,27 +3,33 @@
 #Pull latest Git merges
 git --git-dir=/Docker-Compose/.git --work-tree=/Docker-Compose pull
 
+#Get the directories in /Dcoker-Compose
+DockerFolder="ls -d /Docker-Compose/*"
+
 #Pull latest versions of containers
-docker-compose -f /Docker-Compose/guacozy/docker-compose.yml pull
-docker-compose -f /Docker-Compose/deluge/docker-compose.yml pull
-docker-compose -f /Docker-Compose/nzbget/docker-compose.yml pull
-docker-compose -f /Docker-Compose/ombi/docker-compose.yml pull
-docker-compose -f /Docker-Compose/sonarr/docker-compose.yml pull
-docker-compose -f /Docker-Compose/unifi/docker-compose.yml pull
-docker-compose -f /Docker-Compose/plex/docker-compose.yml pull
-docker-compose -f /Docker-Compose/sftp/docker-compose.yml pull
-docker-compose -f /Docker-Compose/tautulli/docker-compose.yml pull
+forwach n ($DockerFolder) {
+    docker-compose -f $n/docker-compose.yml pull
+}
+#docker-compose -f /Docker-Compose/guacozy/docker-compose.yml pull
+#docker-compose -f /Docker-Compose/deluge/docker-compose.yml pull
+#docker-compose -f /Docker-Compose/nzbget/docker-compose.yml pull
+#docker-compose -f /Docker-Compose/ombi/docker-compose.yml pull
+#docker-compose -f /Docker-Compose/sonarr/docker-compose.yml pull
+#docker-compose -f /Docker-Compose/unifi/docker-compose.yml pull
+#docker-compose -f /Docker-Compose/plex/docker-compose.yml pull
+#docker-compose -f /Docker-Compose/sftp/docker-compose.yml pull
+#docker-compose -f /Docker-Compose/tautulli/docker-compose.yml pull
 
 #Stop running containers
-docker-compose -f /Docker-Compose/guacozy/docker-compose.yml stop
-docker-compose -f /Docker-Compose/deluge/docker-compose.yml stop
-docker-compose -f /Docker-Compose/nzbget/docker-compose.yml stop
-docker-compose -f /Docker-Compose/ombi/docker-compose.yml stop
-docker-compose -f /Docker-Compose/sonarr/docker-compose.yml stop
-docker-compose -f /Docker-Compose/unifi/docker-compose.yml stop
-docker-compose -f /Docker-Compose/plex/docker-compose.yml stop
-docker-compose -f /Docker-Compose/sftp/docker-compose.yml stop
-docker-compose -f /Docker-Compose/tautulli/docker-compose.yml stop
+#docker-compose -f /Docker-Compose/guacozy/docker-compose.yml stop
+#docker-compose -f /Docker-Compose/deluge/docker-compose.yml stop
+#docker-compose -f /Docker-Compose/nzbget/docker-compose.yml stop
+#docker-compose -f /Docker-Compose/ombi/docker-compose.yml stop
+#docker-compose -f /Docker-Compose/sonarr/docker-compose.yml stop
+#docker-compose -f /Docker-Compose/unifi/docker-compose.yml stop
+#docker-compose -f /Docker-Compose/plex/docker-compose.yml stop
+#docker-compose -f /Docker-Compose/sftp/docker-compose.yml stop
+#docker-compose -f /Docker-Compose/tautulli/docker-compose.yml stop
 
 #Update cert.crt and cert.key for Guacozy
 cp /docker/ssh/acme/LetsEncryptTOB.crt /docker/ssh/acme/cert.crt
