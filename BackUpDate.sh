70f32383ccf4fdc886b5f3f9c37ac0c05deedefb
Brent Erickson <brenteri@gmail.com>
2020-04-27 20:06:51 -0500

    Fixed file call and added function

--- BackUpDate.sh
+++ BackUpDate.sh
@@ -1,26 +1,39 @@
 #!/bin/sh
 
+#Define variables
+BackupLocation="/mnt/nas/bk" #Location to backup to
+DockerFolder="/Docker-Compose/*" #Location of Docker-Compose
+Compose="docker-compoase.yml"
+
 #Pull latest Git merges
 git --git-dir=/Docker-Compose/.git --work-tree=/Docker-Compose pull
 
-#Get the directories in /Dcoker-Compose
-DockerFolder="ls -d /Docker-Compose/*"
-
 #Pull latest versions of containers
-for OUTPUT in $DockerFolder; do docker-compose -f $OUTPUT/docker-compose.yml pull; done
+Action="pull"
+Do_Compse
 
 #Stop running containers
-for OUTPUT in $DockerFolder; do docker-compose -f $OUTPUT/docker-compose.yml stop; done
+Action="stop"
+Do_Compse
 
 #Update cert.crt and cert.key for Guacozy
 cp /docker/ssh/acme/LetsEncryptTOB.crt /docker/ssh/acme/cert.crt
 cp /docker/ssh/acme/LetsEncryptTOB.key /docker/ssh/acme/cert.key
 
 #Backup all config directories and files
-rsync --archive --verbose --recursive --checksum /docker/ /mnt/nas/bk
+rsync --archive --verbose --recursive --checksum /docker/ $BackupLocation
 
 #Start all containers
-for OUTPUT in $DockerFolder; do docker-compose -f $OUTPUT/docker-compose.yml up -d; done
+Action="up -d"
+Do_Compse
 
 #Prune old images
 docker image prune -f
+
+function Do_Compose { 
+    # Each time you find docker-compose.yml run action using the set varilable
+    for OUTPUT in $(find $DockerFolder -type f -name $Compose); 
+        do 
+            docker-compose -f $OUTPUT $Action;
+    done
+} #End Do_Compose
