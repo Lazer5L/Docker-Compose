e39b659c54660df640301bf8b918af464f289e7f
Brent Erickson <brenteri@gmail.com>
2020-04-27 20:51:40 -0500

    Updated Backup path

--- BackUpDate.sh
+++ BackUpDate.sh
@@ -9,7 +9,7 @@ function Do_Compose () {
 } #End Do_Compose
 
 #Define variables
-BackupLocation="/mnt/nas/bk" #Location to backup to
+BackupLocation="/mnt/nas/bk/Docker-Compose" #Location to backup to
 DockerFolder="/Docker-Compose/*" #Location of Docker-Compose
 Compose="docker-compose.yml" #Compose file used
 
