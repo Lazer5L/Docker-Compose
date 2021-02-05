d3ab6c16c3309be4dc2237a0d934a26e7edb5f7f
Brent Erickson <brenteri@gmail.com>
2021-02-05 11:15:49 -0600

    Update BackUpDate.sh

--- BackUpDate.sh
+++ BackUpDate.sh
@@ -14,39 +14,39 @@ DockerFolder="/Docker-Compose/*" #Location of Docker-Compose
 Compose="docker-compose.yml" #Compose file used
 
 #Pull latest Git merges
-echo Reaching out to GIT
+echo \n Reaching out to GIT
 git --git-dir=/Docker-Compose/.git --work-tree=/Docker-Compose pull
 
 #Pull latest versions of containers
-echo Pulling Docker images
+echo \n Pulling Docker images
 Do_Compose "pull"
 
 #Notify Discord
-echo Notifying Discord Channel
+echo \n Notifying Discord Channel
 curl -H "Content-Type: application/json" -X POST -d '{"DockerBot": "#general", "content": "Docker is being restarted. Services such as Ombi will be down."}' 'https://discordapp.com/api/webhooks/732244627842793543/pz0yw2i0vedIQoz7TJpGvPUUTFO_vdQvtu_blS9-yeWbzXb8m6FdNnAdFNRLSkqrv9tJ'
 
 #Stop running containers
-echo Stopping Docker images
+echo \n Stopping Docker images
 Do_Compose "stop"
 
 #Update cert.crt and cert.key for Guacozy
-echo Shifting Certs
+echo \n Shifting Certs
 cp /docker/ssh/acme/LetsEncryptTOB.crt /docker/ssh/acme/cert.crt
 cp /docker/ssh/acme/LetsEncryptTOB.key /docker/ssh/acme/cert.key
 cp /docker/ssh/acme/LetsEncryptTOB.all.pem /docker/ssh/acme/cert.pem
 
 #Backup all config directories and files
-echo Backing up container configs
+echo \n Backing up container configs
 rsync --archive --recursive --checksum --delete /docker/ $BackupLocation
 
 #Start all containers
-echo Starting Docker images
+echo \n Starting Docker images
 Do_Compose "up -d"
 
 #Prune old images
-echo Purging Docker images
+echo \n Purging Docker images
 docker image prune -f
 
 #Notify Discord
-echo Notifying Discord
+echo \n Notifying Discord
 curl -H "Content-Type: application/json" -X POST -d '{"DockerBot": "#general", "content": "Docker is starting. Services such as Ombi will be restored in 2 minutes."}' 'https://discordapp.com/api/webhooks/732244627842793543/pz0yw2i0vedIQoz7TJpGvPUUTFO_vdQvtu_blS9-yeWbzXb8m6FdNnAdFNRLSkqrv9tJ'
