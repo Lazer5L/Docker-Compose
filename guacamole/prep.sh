91eecb133cc334dfbfd269cc5380db5821199d25
Brent Erickson <brenteri@gmail.com>
2024-08-28 16:34:09 -0500

    Update prep.sh
    add docker exec command

--- guacamole/prep.sh
+++ guacamole/prep.sh
@@ -1,5 +1,7 @@
 #!/bin/sh
-sudo mkdir /docker/guacamole/config/init >/dev/null 2>&1
-sudo chmod -R +x /docker/guacamole/config/init
+mkdir /docker/guacamole/config/init >/dev/null 2>&1
+chmod -R +x /docker/guacamole/config/init
+cp /Docker-Compose/guavamole/psql.sh /docker/guacamole/config/init/psql.sh
 docker run --rm guacamole/guacamole /opt/guacamole/bin/initdb.sh --postgresql > /docker/guacamole/config/init/initdb.sql
 docker compose -f /Docker-Compose/guacamole/docker-compose.yml up -d postgres
+docker exec postgres BASH /docker-entrypoint-initdb.d/psql.sh
