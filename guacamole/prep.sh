9fce29c8da77cf9046f7612b3d6ecd5bad0fa919
Brent Erickson <brenteri@gmail.com>
2024-08-28 16:53:18 -0500

    Update prep.sh
    bash lowercase

--- guacamole/prep.sh
+++ guacamole/prep.sh
@@ -4,4 +4,4 @@ chmod -R +x /docker/guacamole/config/init
 cp /Docker-Compose/guavamole/psql.sh /docker/guacamole/config/init/psql.sh
 docker run --rm guacamole/guacamole /opt/guacamole/bin/initdb.sh --postgresql > /docker/guacamole/config/init/initdb.sql
 docker compose -f /Docker-Compose/guacamole/docker-compose.yml up -d postgres
-docker exec postgres BASH /docker-entrypoint-initdb.d/psql.sh
+docker exec postgres bash /docker-entrypoint-initdb.d/psql.sh
