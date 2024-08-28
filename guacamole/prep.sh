6bef3bd9733df954356017894c3e4590c1965afa
Brent Erickson <brenteri@gmail.com>
2024-08-28 15:38:46 -0500

    Create prep.sh

---
+++ guacamole/prep.sh
@@ -0,0 +1,5 @@
+#!/bin/sh
+sudo mkdir /docker/guacamole/config/init >/dev/null 2>&1
+sudo chmod -R +x /docker/guacamole/config/init
+docker run --rm guacamole/guacamole /opt/guacamole/bin/initdb.sh --postgres > /docker/guacamole/config/init/initdb.sql
+docker compose -f /Docker-Compose/guacamole/docker-compose.yml up -d
