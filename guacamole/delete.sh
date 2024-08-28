b637230d8ba39ca1a090f93daa1812e299021482
Brent Erickson <brenteri@gmail.com>
2024-08-28 16:36:36 -0500

    Update delete.sh
    rm sudo

--- guacamole/delete.sh
+++ guacamole/delete.sh
@@ -1,3 +1,3 @@
-#!/bin/bash
-sudo chmod -R +x -- /docker/guacamole/config/init
-sudo rm -r -f /docker/guacamole/config/data/ /docker/guacamole/config/drive/ /docker/guacamole/config/record/
+#!/bin/sh
+chmod -R +x -- /docker/guacamole/config/init
+rm -rf /docker/guacamole/config/data/ /docker/guacamole/config/drive/ /docker/guacamole/config/record/
